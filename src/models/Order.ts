ba1acbec1b4ccf2d2763407e7142d4e892839772
lorencete-2003 <l.ramirezcampo@gmail.com>
2024-10-17 08:36:24 +0200

    cambios malos de hace tiempo

---
+++ src/models/Order.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Types } from 'mongoose';
+
+export interface Order{
+  date: Date;
+  address: string;
+  cardHolder: string;
+  cardNumber: string;
+
+}
+
+
+const OrderSchema = new Schema<Order>({
+  date: { type: Date , required:true },
+  address: { type: String, required:true },
+  cardHolder: { type: String, required:true, unique: true },
+  cardNumber: { type: String, required:true, unique: true }
+});
+
+export default mongoose.models.Order || mongoose.model<Order>('Order', OrderSchema);
