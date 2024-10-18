ea30b2e292c0ab82937233f6cd90ff3e11dc835b
lorencete-2003 <l.ramirezcampo@gmail.com>
2024-10-18 17:28:55 +0200

    porque sale el order/route.ts lo estoy borrando

--- src/app/api/orders/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getOrders, GetOrderResponse } from '@/lib/handlers';
-
-export async function GET( request: NextRequest ):
-Promise<NextResponse<GetOrderResponse>> {
-  const orders = await getOrders();
-  return NextResponse.json(orders);
-  
-}
