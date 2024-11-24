6d6bc2bd351067126aaff2bcdb298b93115a302b
lorencete-2003 <l.ramirezcampo@gmail.com>
2024-11-24 17:07:25 +0100

    cambios en el seeder, signin y signout y cart update

--- src/app/(sections)/cart/page.tsx
+++ src/app/(sections)/cart/page.tsx
@@ -83,9 +83,59 @@ export default async function Cart() {
 
               {/* Acciones del Producto */}
               <div className="card-actions w-full flex justify-around">
-                <button className="btn btn-outline btn-base-content">
-                  Update Quantity
-                </button>
+                <div className="relative flex items-center max-w-[8rem]">
+                  <button
+                    type="button"
+                    className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                  >
+                    <svg
+                      className="w-3 h-3 text-gray-900 dark:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 18 2"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M1 1h16"
+                      />
+                    </svg>
+                  </button>
+                  <input
+                    type="number"
+                    id="quantity-input"
+                    value={1} // Valor fijo
+                    readOnly
+                    min="1"
+                    max="50"
+                    className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                    style={{ appearance: 'textfield' }}
+                  />
+                  <button
+                    type="button"
+                    className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                  >
+                    <svg
+                      className="w-3 h-3 text-gray-900 dark:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 18 18"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M9 1v16M1 9h16"
+                      />
+                    </svg>
+                  </button>
+                </div>
+
                 <button className="btn btn-outline btn-error">Eliminar</button>
               </div>
             </div>
