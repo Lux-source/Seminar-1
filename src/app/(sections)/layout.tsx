55c0ab51eb0d270c1367eaa277f099f076ed724e
Lux-source <andrei-lucian@hotmail.com>
2024-11-26 15:28:17 +0100

    Sobraban cosas en el layout principal

--- src/app/(sections)/layout.tsx
+++ src/app/(sections)/layout.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+    <div className="flex min-h-screen flex-col bg-white dark:bg-gray-900">
       <Navbar />
       <Header />
       <main className="mx-auto w-full max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
