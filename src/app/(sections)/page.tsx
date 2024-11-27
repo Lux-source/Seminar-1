62216e47c060569e6a41eca190a9d34eb55c0250
Lux-source <andrei-lucian@hotmail.com>
2024-11-27 19:19:21 +0100

    Ejemplo seminario

--- src/app/(sections)/page.tsx
+++ src/app/(sections)/page.tsx
@@ -1,4 +1,5 @@
 import ProductTile from '@/components/ProductTile'
+import SampleClientComponent from '@/components/SampleClientComponent'
 import { getProducts } from '@/lib/handlers'
 
 export default async function Index() {
@@ -6,6 +7,7 @@ export default async function Index() {
 
   return (
     <div className="flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+      <SampleClientComponent></SampleClientComponent>
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
       {data.products.map((product) => (
           <ProductTile key={product._id.toString()} product={product} />
