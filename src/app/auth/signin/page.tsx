8aa2f6ba524ede91277072a0986f5730e463d0b7
Lux-source <andrei-lucian@hotmail.com>
2024-12-06 18:44:21 +0100

    Cambios en el sigin para que se vea también en modo oscuro

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,26 +3,26 @@ import Link from 'next/link'
 
 export default function SignIn() {
   return (
-    <div className='flex w-full flex-col px-6 py-12'>
-      <div className='mx-auto w-full max-w-sm'>
-        <img
+    <div className="mx-auto max-w-3xl rounded-lg bg-white p-4 shadow-md dark:bg-gray-900 sm:p-6 lg:p-8">
+      <div className="mx-auto w-full max-w-sm text-center">
+      <img
           className='mx-auto h-10 w-auto'
           src='/img/logo.svg'
           alt='GameShop logo'
         />
-        <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
-          Sign in to your account
+        <h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100">
+        Sign in to your account
         </h2>
       </div>
 
-      <div className='mx-auto mt-10 w-full max-w-sm'>
+      <div className="mx-auto mt-10 w-full max-w-sm">
         <SignInForm />
 
-        <p className='mt-10 text-center text-sm text-gray-500'>
+        <p className="mt-10 text-center text-sm text-gray-500 dark:text-gray-400">
           Not a member?{' '}
           <Link
             href='/auth/signup'
-            className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300"
           >
             Register now!
           </Link>
--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -50,7 +50,7 @@ export default function SignInForm() {
       <div>
         <label
           htmlFor='email'
-          className='block text-sm font-medium leading-6 text-gray-900'
+          className="block text-sm font-medium leading-6 text-gray-900 dark:text-gray-300"
         >
           E-mail address
         </label>
@@ -61,7 +61,7 @@ export default function SignInForm() {
           autoComplete='email'
           placeholder='johndoe@example.com'
           required
-          className='peer mt-2 block w-full rounded-md border-0 px-1.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:ring-red-500'
+          className="peer mt-2 block w-full rounded-md border-0 px-1.5 py-2 text-gray-900 dark:text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 placeholder:text-gray-400 dark:placeholder-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:ring-red-500"
           value={formValues.email}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setFormValues((prevFormValues) => ({
@@ -78,7 +78,7 @@ export default function SignInForm() {
       <div>
         <label
           htmlFor='password'
-          className='block text-sm font-medium leading-6 text-gray-900'
+          className="block text-sm font-medium leading-6 text-gray-900 dark:text-gray-300"
         >
           Password
         </label>
@@ -89,7 +89,7 @@ export default function SignInForm() {
           autoComplete='current-password'
           placeholder=' '
           required
-          className='peer mt-2 block w-full rounded-md border-0 px-1.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:ring-red-500'
+          className="peer mt-2 block w-full rounded-md border-0 px-1.5 py-2 text-gray-900 dark:text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 placeholder:text-gray-400 dark:placeholder-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:ring-red-500"
           value={formValues.password}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setFormValues((prevFormValues) => ({
@@ -104,15 +104,15 @@ export default function SignInForm() {
       </div>
 
       <div className={error ? '' : 'hidden'}>
-        <p className='mt-2 rounded-md border-0 bg-red-500 bg-opacity-30 px-3 py-1.5 text-sm text-gray-900 ring-1 ring-inset ring-red-500'>
-          {error}
+      <p className="mt-2 rounded-md border-0 bg-red-500 bg-opacity-30 px-3 py-1.5 text-sm text-gray-900 dark:text-gray-100 ring-1 ring-inset ring-red-500">
+      {error}
         </p>
       </div>
 
       <div>
         <button
           type='submit'
-          className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 group-invalid:pointer-events-none group-invalid:opacity-30'
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 group-invalid:pointer-events-none group-invalid:opacity-30 dark:bg-indigo-500 dark:hover:bg-indigo-400"
         >
           Sign in
         </button>
